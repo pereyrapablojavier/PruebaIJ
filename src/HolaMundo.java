86705666ab908190344ba8f71fdabf9d993675b6
Pablo <pabloporra@gmail.com>
2020-03-19 17:19:37 -0300

    Initial commit

---
+++ src/HolaMundo.java
@@ -0,0 +1,18 @@
+public class HolaMundo {
+
+    /*
+    Aca va un comentario
+     */
+    public static void main(String[] args) {
+        ejecutar();
+    }
+
+    public static void ejecutar() {
+        miToString();
+    }
+
+
+    public static void miToString() {
+        System.out.println(String.format("Este es mi mensaje %s", "Este es mi String"));
+    }
+}
